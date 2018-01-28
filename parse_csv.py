5a0be6ce69422293bcac49087eb98ba892a6250b
Patrick McNally <Patrick@pjmcnally.net>
2018-01-27 18:14:48 -0600

    Move parse_date_str function into class. Add ability to print dates.

--- parse_csv.py
+++ parse_csv.py
@@ -14,38 +14,14 @@ def main():
         event_obj = event(*row)
         events.append(event_obj)
         print(event_obj)
+        event_obj.print_dates()
 
-def parse_date(date_str):
-    """ Takes date string and returns list of dates.
 
-        For example Jan 4-5 will return [Jan 4, Jan 5].
-        Each item in the list will be a datetime object
-    """
-    dates = []
-    date_format = "%b %d, %Y"
-
-    if date_str == "TBD":
-        return dates
-    elif "-" in date_str:
-        p = "(?P<mon>\w{3})\s*(?P<beg>\d{1,2})-(?P<end>\d{1,2})"
-        match = re.search(p, date_str)
-        month = match["mon"]
-        begin = int(match["beg"])
-        end = int(match["end"])
-        for num in range(begin, end + 1):
-            new_date_str = "{} {}, 2018".format(month, num)
-            date = datetime.strptime(new_date_str, date_format)
-            dates.append(date)
-    else:
-        date_str += ", 2018"
-        date = datetime.strptime(date_str, date_format)
-        dates.append(date)
-
-    return dates
 
 class event():
     def __init__(self, date_str, name, divison, location, contact, email, website, audl):
         self.date_str = date_str
+        self.dates = self.parse_date_str(date_str)
         self.name = name
         self.division = divison
         self.location = location
@@ -57,7 +33,6 @@ class event():
     def __str__(self):
         basic_info = "\r\n{} {} at {} for {} players.".format(
             self.date_str,
-            #self.date.strftime("%b %d, %Y"),
             self.name,
             self.location,
             self.division,
@@ -81,5 +56,38 @@ class event():
             contact_info,
             web_info
         )
+
+    def parse_date_str(self, date_str):
+        """ Takes date string and returns list of dates.
+
+            For example Jan 4-5 will return [Jan 4, Jan 5].
+            Each item in the list will be a datetime object
+        """
+        dates = []
+        date_format = "%b %d, %Y"
+
+        if date_str == "TBD":
+            return dates
+        elif "-" in date_str:
+            p = "(?P<mon>\w{3})\s*(?P<beg>\d{1,2})-(?P<end>\d{1,2})"
+            match = re.search(p, date_str)
+            month = match["mon"]
+            begin = int(match["beg"])
+            end = int(match["end"])
+            for num in range(begin, end + 1):
+                new_date_str = "{} {}, 2018".format(month, num)
+                date = datetime.strptime(new_date_str, date_format)
+                dates.append(date)
+        else:
+            date_str += ", 2018"
+            date = datetime.strptime(date_str, date_format)
+            dates.append(date)
+
+        return dates
+
+    def print_dates(self):
+        for date in self.dates:
+            print(date.strftime("%b %d, %Y"))
+
 
 main()
