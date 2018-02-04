9d447ac2e972c36f1bbc6f54f9fb5d0bfd8e7bde
Patrick McNally <Patrick@pjmcnally.net>
2018-02-03 20:49:50 -0600

    First success creating event.

--- quickstart.py
+++ quickstart.py
@@ -102,9 +102,36 @@ def main():
     # Create service object
     service = discovery.build('calendar', 'v3', http=http)
 
+    event = {
+      'summary': 'Patrick\'s Test Event',
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'date': '2018-02-06',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'end': {
+        'date': '2018-02-08',
+        'timeZone': 'America/Los_Angeles',
+      },
+      # 'recurrence': [
+      #   'RRULE:FREQ=DAILY;COUNT=2'
+      # ],
+      # 'attendees': [
+      #   {'email': 'lpage@example.com'},
+      #   {'email': 'sbrin@example.com'},
+      # ],
+      # 'reminders': {
+      #   'useDefault': False,
+      #   'overrides': [
+      #     {'method': 'email', 'minutes': 24 * 60},
+      #     {'method': 'popup', 'minutes': 10},
+      #   ],
+      # },
+    }
 
-
-
+    event = service.events().insert(calendarId=calendar, body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
 
 
 if __name__ == '__main__':
